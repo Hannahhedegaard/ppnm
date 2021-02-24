df74c146e9185dfb990d737e94072a2eed0f8625
Hannahhedegaard <h.hedegaard@hotmail.com>
2021-02-24 10:12:53 +0100

    ex. plot

---
+++ pyxplot/gamma/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+#include<gsl/gsl_sf_gamma.h>
+
+double Gamma(double);
+
+int main(){
+	double xmin=1, xmax=4;
+	for(double x=xmin; x<=xmax; x+=1.0/8){
+		printf("%10g %10g %10g %10g\n", x, tgamma(x), gsl_sf_gamma(x), Gamma(x));
+		//printf("%10g\n", x);
+		}
+return 0;
+}
