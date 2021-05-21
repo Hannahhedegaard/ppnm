5d90124237565c5dd7f09731dd3dee50bc6af488
Hannahhedegaard <h.hedegaard@hotmail.com>
2021-05-21 15:01:04 +0200

    We think its done

--- homework6_numericintegration/functions.c
+++ homework6_numericintegration/functions.c
@@ -3,62 +3,58 @@
 #include <assert.h>
 
 double global_error;
-int calls;
+//int calls;
 
-double integrate(double f(double x), double a, double b, double acc, double eps, double f2, double f3, int nrec)
+double integrate(double fg(double f(double x), double x, double a), double f(double x), double a0, double a, double b, double acc, double eps, double f2, double f3, int nrec)
 {
 	assert(nrec<1e6);
-	printf("test = %g\n", f(1.0));
-	//double f1=f(a+(b-a)/6.0), f4=f(a+5.0*(b-a)/6.0);
-	//double Q=(2.0*f1+f2+f3+2.0*f4)/6.0*(b-a), q=(f1+f4+f2+f3)/4.0*(b-a);
-	//double tolerance=acc+eps*fabs(Q), error=fabs(Q-q);
-	//if (error < tolerance) {
-	//	global_error += pow(error,2);
-	//	return Q;
-	//}
-	//else {return integrate(f,a,(a+b)/2.0,acc/sqrt(2),eps, f1, f2, nrec+1)+
-	//			integrate(f,(a+b)/2.0,b,acc/sqrt(2),eps, f3, f4, nrec+1);
-	//}
-	return 13;
+	double f1=fg(f, a+(b-a)/6.0, a0), f4=fg(f, a+5.0*(b-a)/6.0, a0);
+	double Q=(2.0*f1+f2+f3+2.0*f4)/6.0*(b-a), q=(f1+f4+f2+f3)/4.0*(b-a);
+	double tolerance=acc+eps*fabs(Q), error=fabs(Q-q);
+	if (error < tolerance) {
+		global_error += pow(error,2);
+		return Q;
+	}
+	else {return integrate(fg, f, a0, a,(a+b)/2.0,acc/sqrt(2.),eps, f1, f2, nrec+1)+
+				integrate(fg, f,a0,(a+b)/2.0,b,acc/sqrt(2.),eps, f3, f4, nrec+1);
+	}
 }
 
-double adapt(double f(double), double a, double b, double acc, double eps){
-	global_error = 0.0;
-	//double f2=f(a+2.0*(b-a)/6.0), f3=f(a+4.0*(b-a)/6.0); int nrec=0;
-	//printf("f2 = %g\n", f2);
-	//double estimate = integrate(f, a, b, acc, eps, f2, f3, nrec);
-	printf("error = %g\n", sqrt(global_error));
-	return 0;//estimate;
+double g0(double f(double x), double x, double a){
+	return f(x);
 }
 
-double infintegrate(double f(double), double a, double b, double acc, double eps){
-	if (isinf(a)==-1 && isinf(b)==1){
-		double g(double x){
-			calls++;
+double g1(double f(double x), double x, double a){
 			return (f((1-x)/x) + f(-(1.0-x)/x))*1.0/pow(x,2);
 		}
-		
-		return adapt(g, 0.0, 1.0, acc, eps);
-	}
-	else if (isinf(b)==1){
-		double g(double x){
-			calls++;
+
+double g2(double f(double x), double x, double a){
 			return f(a + (1.0 - x)/x)/(x*x);
 		}
-		double A = 0.0, B = 1.0;
-		double f2=g(A+2.0*(B-A)/6.0), f3=g(A+4.0*(B-A)/6.0); int nrec=0;
-		printf("f2=%g, f3=%g\n", f2,f3);
-		printf("hej\n");
-		double Result = integrate(g, A, B, acc, eps, f2, f3, nrec);
-		printf("result = %g\n", Result);
-		return 0;
-	}
-	else if (isinf(a)==-1){
-		double g(double x){
-			calls++;
+
+double g3(double f(double x), double x, double b){
 			return f(b - (1.0-x)/x)*1.0/pow(x,2);
 		}
-		return adapt(g, 0.0, 1.0, acc, eps);
+
+double infintegrate(double f(double x), double a, double b, double acc, double eps){
+	global_error = 0;
+	if (isinf(a)==-1 && isinf(b)==1){
+		double A = 0.0, B = 1.0;
+		double f2=g1(f, A+2.0*(B-A)/6.0, 0), f3=g1(f, A+4.0*(B-A)/6.0, 0); int nrec=0;
+		return integrate(g1,f, a, A, B, acc, eps, f2, f3, nrec);
 	}
-return adapt(f, a, b, acc, eps);
+	else if (isinf(b)==1){
+		double A = 0.0, B = 1.0;
+		double f2=g2(f, A+2.0*(B-A)/6, a), f3=g2(f, A+4.0*(B-A)/6, a); int nrec=0;
+		return integrate(g2,f, a, A, B, acc, eps, f2, f3, nrec);
+	}
+	else if (isinf(a)==-1){
+		double A = 0.0, B = 1.0;
+		double f2=g3(f, A+2.0*(B-A)/6.0, b), f3=g3(f, A+4.0*(B-A)/6.0, b); int nrec=0;
+		return integrate(g3,f, b, A, B, acc, eps, f2, f3, nrec);
+	}
+	else {
+		double f2=g0(f, a+2.0*(b-a)/6.0, 0), f3=g0(f, a+4.0*(b-a)/6.0, 0); int nrec=0;
+		return integrate(g0,f, a, a, b, acc, eps, f2, f3, nrec);
+	}
 }
--- homework6_numericintegration/main.c
+++ homework6_numericintegration/main.c
@@ -3,41 +3,67 @@
 #include <assert.h>
 #include <gsl/gsl_integration.h>
 
-double adapt(double f(double), double a, double b, double acc, double eps);
-double infintegrate(double f(double), double a, double b, double acc, double eps);
+double adapt(double f(double x), double a, double b, double acc, double eps);
+double infintegrate(double f(double x), double a, double b, double acc, double eps);
 
 int calls=0;
 double fu(double x){calls++; return 2.0/(1.0 + x*x);};
+double gsl_fu(double x, void * params){calls++; return 2.0/(1.0 + x*x);};
 //double f2(double x, void * params){
 //	double f2 = 4*sqrt(1-pow(x,2));
 //	return f2;
 //};
 
-//double g(double x){calls++; return f(cos(x))*sin(x);}; 	
+double fb(double x){calls++; return 4*sqrt(1-x*x);};
 
-double g2(double x, void * params){
-	double g2 = fu(cos(x))*sin(x);
-	return g2;
+double g(double x){return fb(cos(x))*sin(x);}; 	
+double fuu(double x){
+			calls++;
+			return 2.0/(1.0 + pow((0 + (1.0 - x)/x),2)/(x*x));
+		}
+
+
+double g_gsl(double x, void * params){
+	double g_gsl = fb(cos(x))*sin(x);
+	return g_gsl;
 }; 	
 
 int main(){
-	double a=0.0, b=INFINITY; 
-	double acc=0.01, eps=0.01;
-	double Q = infintegrate(fu,a,b,acc,eps);
-	printf("Q=%.25g calls=%d\n", Q, calls);
-	//calls = 0;
-	//a = acos(b); // Laver variable om
-	//b = acos(a);
-	//double Qcc = adapt(g,a,b,acc,eps);
-	//printf("Qcc=%.25g calls=%d\n", Qcc, calls);
-	//size_t size = 10000;
-	//double result, abserr;
-	//calls = 0;
-	//gsl_function F;
-    //F.function=&g2;
-	//gsl_integration_workspace * w = gsl_integration_workspace_alloc(size);
-	//gsl_integration_qags(&F, a, b, acc, eps, size, w, &result, &abserr);
-	//printf("gsl=%.25g calls=%d\n", result, calls);
+	// opgave A
+	double a=0, b=1; 
+	double acc=1e-6, eps=1e-6;
+	double Q1 = infintegrate(fuu,a,b,acc,eps); 
+	printf("Q1=%.25g calls=%d\n", Q1, calls);
+	
+	// Opgave B
+	calls = 0;
+	a = 0; b = 1;
+	a = acos(b); // Laver variable om
+	b = acos(a); // Bytter om på grænserne fordi dx = -sinθdθ (se billede + afl-beskrivelse) - gælder uanset hvad a og b er (ikke inf)
+	printf("a = %g, b = %g\n", a, b);
+	double Qcc = infintegrate(g,a,b,acc,eps); 
+	printf("Qcc=%.25g calls=%d\n", Qcc, calls);
+	size_t size = 10000;
+	double result, abserr;
+	//sammenlign med GSL
+	calls = 0;
+	gsl_function F;
+    F.function=&g_gsl;
+	gsl_integration_workspace * w = gsl_integration_workspace_alloc(size);
+	gsl_integration_qags(&F, a, b, acc, eps, size, w, &result, &abserr);
+	printf("gsl=%.25g calls=%d\n", result, calls);
+	
+	//Opgave C
+	calls = 0;
+	b = INFINITY;
+	double Q_inf = infintegrate(fu,a,b,acc,eps);
+	printf("Q_inf = %.25g calls=%d\n", Q_inf, calls);
+	//sammenlign med GSL
+	calls = 0;
+    F.function=&gsl_fu;
+	gsl_integration_qagiu(&F, a, acc, eps, size, w, &result, &abserr);
+	printf("gsl=%.25g calls=%d\n", result, calls);
+	
 return 0;
 }
 
